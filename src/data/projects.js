0c439e8277d406164001b13ceb9baa6a3743768e
Tal <trimoni8@gmail.com>
2022-10-26 09:41:19 -0400

    tic tac toe add

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,18 +1,23 @@
-
-
-
 const projects = [
 {
   title: 'ColorFul Connect Four',
-  description: 'ColorFul Connect Four is basically Connect Four but Colorful',
+  description: `ColorFul Connect Four is basically Connect Four but Colorful`,
   image: '/color-connect-four2.jpg',
   repositoryLink: 'https://github.com/trimoni/Connect-Four-Unit-1.git',
   depositoryLink: 'https://colorfulconnectfour.netlify.app/'
 },
 
 {
+  title: 'Weather Tic-Tac-Toe',
+  description: `I created a game of Tic-Tac-Toe where it's Sun VS Moon with some weather elements mixed in`,
+  image: '/tictactoe.jpg',
+  repositoryLink: 'https://github.com/trimoni/ttt-weekend.git',
+  depositoryLink: 'https://weather-tic-tac-toe.netlify.app/'
+},
+
+{
   title: 'Event Planner',
-  description: 'Event Planner is an app where users can create events!',
+  description: `Event Planner is an app where users can create events!`,
   image: '/event-planner.jpg',
   repositoryLink: 'https://github.com/trimoni/Event-Planning.git',
   depositoryLink: 'https://eventplanner-rimoni.fly.dev/'
