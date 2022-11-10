8f7aae88966e3d8d65687ebf880824b04bc620c7
Tal <trimoni8@gmail.com>
2022-11-10 10:15:03 -0500

    resume

--- src/pages/ProjectDetails/ProjectDetails.jsx
+++ src/pages/ProjectDetails/ProjectDetails.jsx
@@ -1,20 +1,24 @@
 import findProject from "../../utilities/findProject";
 import { useLocation } from "react-router-dom";
-
+import styles from "./ProjectDetails.module.css";
 
 const ProjectDetails = () => {
-  const location = useLocation()
-  const project = findProject(location.pathname)
+  const location = useLocation();
+  const project = findProject(location.pathname);
   return (
-    <>
-    <h1>{project.title}</h1>
-    <p>{project.description}</p>
-    <img src={project.image} alt="connect four" />
-    <br />
-    <button><a href={project.repositoryLink}>GitHub</a></button>
-    <button><a href={project.depositoryLink}>Website</a></button>
-    </>
+    <body className={styles.container}>
+      <h1>{project.title}</h1>
+      <p>{project.description}</p>
+      <img src={project.image} alt="connect four" />
+      <br />
+      <button>
+        <a href={project.repositoryLink}>GitHub</a>
+      </button>
+      <button>
+        <a href={project.depositoryLink}>Website</a>
+      </button>
+    </body>
   );
-}
+};
 
 export default ProjectDetails;
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,9 +1,19 @@
+import styles from '../styles/Resume.module.css'
+import resume from '../public/Resume.pdf'
+
 const Resume = () => {
   return (
-    <>
-      <h3>Download Resume</h3>
-      <a href="TalRimoniResume.pdf" download>Resume</a>
-    </>
+    <main className={styles.container}>
+      <embed 
+        type="application/pdf" 
+        src={resume}
+        height="90%"
+        width="80%"
+        />
+          <div className="buttonDiv">
+        <a className="btn btn-dark" href={require({resume})} download="Tal Rimoni's Resume">Download Resume</a>
+      </div>
+    </main>
   );
 }
 
