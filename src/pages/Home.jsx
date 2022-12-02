5b998fd9ee69a9714b69204758f9ca64edfec9cc
Tal <trimoni8@gmail.com>
2022-12-02 12:41:19 -0500

    edit home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,9 +4,12 @@ import styles from '../styles/Home.module.css'
 const Home = () => {
   return (
     <main className={styles.container}>
-      <h2>Software Engineer</h2>
+      <div className={styles.align}>
+        <h2>Tal Rimoni</h2>
+        <h3>Aspiring Software Engineer!</h3>
+      </div>
     <img src={Tal} alt="headshot of tal" />
-      <p>I'm a graduate from General Assembly's Software Engineer Intensive program! I specialize in Front and Back end work, building websites and apps from scratch. I believe good communication and collaboration gets the job done!</p>
+      <p className={styles.paragraph}>I'm a graduate from General Assembly's Software Engineer Intensive program! I specialize in Front and Back end work, building websites and apps from scratch. I believe good communication and collaboration gets the job done!</p>
     </main>
   );
 }
