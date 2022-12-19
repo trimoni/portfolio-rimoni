c4753623ddac3d0e775ae6a15f969a5e8c41d706
Tal <trimoni8@gmail.com>
2022-12-19 11:35:13 -0500

    fix desc

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,7 +1,7 @@
 const projects = [
 {
   title: 'ColorFul Connect Four',
-  description: `A simple game of Connect Four that was built using HTML, CSS & JavaScript`,
+  description: `HTML, CSS & JavaScript`,
   image: '/color-connect-four2.jpg',
   repositoryLink: 'https://github.com/trimoni/Connect-Four-Unit-1.git',
   depositoryLink: 'https://colorfulconnectfour.netlify.app/'
@@ -9,7 +9,7 @@ const projects = [
 
 {
   title: 'Weather Tic-Tac-Toe',
-  description: `A straight-forward game of TicTacToe built with using HTML, CSS & JavaScript`,
+  description: `HTML, CSS & JavaScript`,
   image: '/tictactoe.jpg',
   repositoryLink: 'https://github.com/trimoni/ttt-weekend.git',
   depositoryLink: 'https://weather-tic-tac-toe.netlify.app/'
@@ -17,7 +17,7 @@ const projects = [
 
 {
   title: 'Event Planner',
-  description: `A MERN Stack application that uses Express & Node.Js that allows you to plan events`,
+  description: `Express & Node.Js`,
   image: '/event-planner.jpg',
   repositoryLink: 'https://github.com/trimoni/Event-Planning.git',
   depositoryLink: 'https://eventplanner-rimoni.fly.dev/'
@@ -25,7 +25,7 @@ const projects = [
 
 {
   title: 'myProps',
-  description: `A MERN stack application that uses React to track property user owns`,
+  description: `React & CSS`,
   image: '/myProps.jpg',
   repositoryLink: 'https://github.com/trimoni/myProps-front-end.git',
   depositoryLink: 'https://mypropsmanagement.netlify.app/'
@@ -33,7 +33,7 @@ const projects = [
 
 {
   title: 'Pokemon Card Collector',
-  description: `A MERN Stack application collecting Pokemon Cards using Python & Django`,
+  description: `Python & Django`,
   image: '/pokemoncollector.png',
   repositoryLink: 'https://github.com/trimoni/pokemon-card-collector.git',
   depositoryLink: 'https://pokemon-card-collector.herokuapp.com/'
@@ -41,7 +41,7 @@ const projects = [
 
 {
   title: 'Wordle',
-  description: `Made with HTML, CSS, JS`,
+  description: `HTML, CSS, JS`,
   image: '/wordle.png',
   repositoryLink: 'https://github.com/trimoni/wordle-js.git',
   depositoryLink: 'https://wordle-free.netlify.app/'
