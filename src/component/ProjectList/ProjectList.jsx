583954b9be6a0bfde49d785884689a55fb37cd55
Tal <trimoni8@gmail.com>
2023-01-18 13:57:52 -0500

    fix links in buttons

--- src/component/ProjectList/ProjectList.jsx
+++ src/component/ProjectList/ProjectList.jsx
@@ -7,7 +7,7 @@ const ProjectList = (props) => {
     <>
     <section className={styles.projectContainer}>
       {props.projects.map((project, i) =>
-      <ProjectPreview title={project.title} key={i} image={project.image} description={project.description}/>
+      <ProjectPreview title={project.title} key={i} image={project.image} description={project.description} repositoryLink={project.repositoryLink} depositoryLink={project.depositoryLink}/>
       )}
     </section>
     </>
